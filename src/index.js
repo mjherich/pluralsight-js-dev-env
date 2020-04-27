53c774d255533ec98b7272d2d14b19309fb9e5d0
Matt Herich <mjherich@gmail.com>
2020-04-27 15:33:14 -0700

    Add /users endpoint, update index.html, fix tests

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import numeral from 'numeral';
+import { getUsers } from "./api/userApi";
 
-import './index.css';
 
-const courseValue = numeral(1000).format('$0,0.00');
-console.log(`I would pay ${courseValue} for this awesome course!`);  // eslint-disable-line no-console
+getUsers().then(res => {
+  let usersBody = "";
+
+  res.forEach(user => {
+    usersBody += `<tr>
+      <td><a href="#" data-id="${user.id}" class="delete-user">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+    </tr>`
+  });
+
+  global.document.getElementById('users').innerHTML = usersBody;
+});
