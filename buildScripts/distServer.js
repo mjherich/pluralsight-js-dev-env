0b98aa4a7676c85264e6236f53a60bf26c8cc560
Matt Herich <mjherich@gmail.com>
2020-05-04 08:55:30 -0700

    Update production url and clean up distServer

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -15,14 +15,6 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 })
 
-app.get('/users', function(req, res) {
-  res.json([
-    {"id": 1, "firstName": "Matt", "lastName": "Herich", "enail": "email1@test.com"},
-    {"id": 2, "firstName": "Bob", "lastName": "Doe", "enail": "email2@test.com"},
-    {"id": 3, "firstName": "Alice", "lastName": "Brown", "enail": "email3@test.com"}
-  ]);
-});
-
 app.listen(PORT, function(err) {
   if (err) {
     console.log(err);
